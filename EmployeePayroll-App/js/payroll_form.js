d2f61fe2c6920d6d25719c16083f6d3887d53714
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-23 22:19:04 +0530

    [Mihir] Refactor. Use of DOM content loaded in name and salary

--- EmployeePayroll-App/js/payroll_form.js
+++ EmployeePayroll-App/js/payroll_form.js
@@ -1,18 +1,21 @@
+window.addEventListener('DOMContentLoaded',(event) =>{
+    const name = document.querySelector('#name');
+    const nameError = document.querySelector('#name-error');
+    name.addEventListener('input', function () {
+        let namRegex = new RegExp(/^[A-Z][a-z]{2,}$/);
+        if (namRegex.test(name.value))
+            nameError.textContent = "";
+        else
+            nameError.textContent = "Name is Incorrect";
+    })});
+    
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
 salary.addEventListener('input', function () {
     output.textContent = salary.value;
 });
-const name = document.querySelector('#name');
-const nameError = document.querySelector('#name-error');
-name.addEventListener('input', function () {
-    let namRegex = new RegExp(/^[A-Z][a-z]{2,}$/);
-    if (namRegex.test(name.value))
-        nameError.textContent = "";
-    else
-        nameError.textContent = "Name is Incorrect";
-});
+
 function save() {
     try {
         createEmployeePayroll();
