2a30c26a512bed0b5ea6e72d21b752d2e23c841d
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-23 21:58:22 +0530

    [Mihir] Add. Validation of fields on getters in employee_payroll javascript file

--- EmployeePayroll-App/js/Employee_Payroll.js
+++ EmployeePayroll-App/js/Employee_Payroll.js
@@ -1,16 +1,65 @@
-const text = document.querySelector('#name');
-const textError = document.querySelector('.text-error');
-text.addEventListener('input', function () {
-    let namRegex = new RegExp(/^[A-Z][a-z]{2,}$/);
-    if (namRegex.test(text.value))
-        textError.textContent = "";
-    else
-        textError.textContent = "Name is incorrect";
-})
+class PayrollModel {
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
+        if (nameRegex.test(name))
+            this._name = name;
+        else {
+            throw "Name is Incorrect";
+        }
+    }
+    get profile() {
+        return this._profile;
+    }
+    set profile(profile) {
+        this._profile = profile;
+    }
+    get gender() {
+        return this._gender;
+    }
+    set gender(gender) {
+        this._gender = gender;
+    }
+    get department() {
+        return this._department;
+    }
+    set department(department) {
+        this._department = department;
+    }
+    get salary() {
+        return this._salary;
+    }
+    set salary(salary) {
+        this._salary = salary;
+    }
 
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function () {
-    output.textContent = salary.value;
-})
+    get startDate() {
+        return this._startDate;
+    }
+    set startDate(startDate) {
+        if (startDate <= new Date())
+            this._startDate = startDate.toLocaleString(undefined, {
+                timeZone: 'Asia/Kolkata'
+            });
+        else {
+            throw "Invalid Start Date";
+        }
+    }
+    get note() {
+        return this._notes;
+    }
+    set note(note) {
+        this._note = note;
+    }
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+    toString() {
+        return "Id = " + this.id + "\nName = " + this.name + "\nSalary = " + this.salary + "\nGender = " + this.gender + " \nStartdate = " + this.startDate + "\nDepartments = " + this.department + "\nProfile = " + this.profile + "\nNote = " + this.note;
+    }
+}
