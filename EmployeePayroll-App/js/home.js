832e6048b160326a3ee38775b8625265b62ae9f0
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-25 23:10:49 +0530

    [Mihir] Add. Ability to view details from js file

---
+++ EmployeePayroll-App/js/home.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    createInnerHTML();
+  });
+  
+  const createInnerHTML = () => {
+    const headerHTML = `
+      <th></th>
+      <th>Name</th>
+      <th>Gender</th>
+      <th>Department</th>
+      <th>Salary</th>
+      <th>Start Date</th>
+      <th>Actions</th>`;
+    const innerHtml = `${headerHTML}
+      <tr>
+        <td><img class="profile" alt="" src="../assets//profile-images/Ellipse -2.png"></td>
+        <td>Mayank Khatri</td>
+        <td>Male</td>
+        <td><div class="dept-label">HR</div>
+            <div class="dept-label">Finance</div></td>
+        <td>300000</td>
+        <td>1 Nov 2020</td>
+        <td>
+          <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+          <img id="1" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
+        </td>
+      </tr>`;
+    document.querySelector('#table-display').innerHTML = innerHtml;
+  };
